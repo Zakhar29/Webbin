7e38f3e5ed16cda5ed7078e6ba475f4d65151e71
Zakhar29 <125102570+Zakhar29@users.noreply.github.com>
2025-01-21 11:30:21 +1000

    Фронт
    Каталог, главная страница

---
+++ core/templates/js-code/code.js
@@ -0,0 +1,14 @@
+
+
+new Swiper('.image-slider',{
+  // Стрелки
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
+  autoheight: true,
+  slidesPerView: 3, //количество показов картинок на слайдере которые отображаются одновременно
+  spaceBetween: 10,
+  slidesPerGroup: 1,
+});
+//
